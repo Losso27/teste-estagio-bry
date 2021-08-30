6c6b82021b9e6a9b10a4a744c6164e88d15a1ddf
felipe.losso <felipe.losso@use-move.com>
2021-08-29 21:41:56 -0300

    Etapa 1

--- etapa1/src/Main.java
+++ etapa1/src/Main.java
@@ -13,18 +13,21 @@ import java.util.Collections;
 
 public class Main {
     public static void main(String[] args) throws IOException, NoSuchAlgorithmException {
+        //Input do arquivo doc.txt
         File file = new File("./resources/doc.txt");
         FileInputStream stream = new FileInputStream(file);
 
+        //Geração do resumo criptografico
         MessageDigest digest = MessageDigest.getInstance("SHA-256");
-
         byte[] bytes = digest.digest(stream.readAllBytes());
 
+        //Conversão do resumo criptografico para hexadecimal
         StringBuilder sb = new StringBuilder();
         for(byte b: bytes) {
             sb.append(String.format("%02x", b));
         }
 
+        //Escrita do Hexa decimal em um arquivo de output
         Path output = Paths.get("./resources/output.txt");
         Files.write(output, Collections.singleton(sb.toString()), StandardCharsets.UTF_8);
     }
